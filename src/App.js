fda23f08b1bd90992bb5ee7c4de617823484e6c2
Nathan Brown <brown.nathanjames@gmail.com>
2020-08-25 00:05:24 +1000

    Add list feature to show list of resources
    Added list feature to show list of resources. Created test array of resources to test. Added sorting by rating descending to list.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,56 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      resources: [
+        {
+          title: 'CSS Tricks - A Complete Guide to Flexbox',
+          url: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/',
+          tags: ['css', 'front end'],
+          rating: 7
+        },
+        {
+          title: 'freecodecamp',
+          url: 'https://www.freecodecamp.org/',
+          tags: ['course', 'HTML', 'css', 'javascript', 'front end', 'back end'],
+          rating: 10
+        },
+        {
+          title: 'CodeAcademy',
+          url: 'https://www.codecademy.com/',
+          tags: ['course', 'HTML', 'css', 'javascript', 'front end', 'back end'],
+          rating: 5
+        },
+        {
+          title: 'Eloquent Javascript',
+          url: 'https://eloquentjavascript.net/',
+          tags: ['book', 'javascript'],
+          rating: 2
+        },
+        {
+          title: 'Art Of Node',
+          url: 'https://github.com/maxogden/art-of-node',
+          tags: ['nodejs', 'github repository'],
+          rating: 3
+        },
+        {
+          title: 'API list',
+          url: 'https://apilist.fun/',
+          tags: ['APIs'],
+          rating: 1
+        }
+      ]
+    }
+  }
+
+  sortByRating = (a, b) => {
+    if(a.rating > b.rating) return -1;
+    if(a.rating < b.rating) return 1;
+    return 0;
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,8 +63,19 @@ class App extends React.Component {
           <p>Search</p>
           <p>Favourites</p>
         </nav>
-        <main>
-          Content to come soon!
+        <main className="main">
+          <ul className="main__list">
+          {
+            this.state.resources.sort(this.sortByRating).map((res,i) => {
+              return (
+                <li className="main__list-item" key={i} >
+                  <p>{res.title}</p>
+                  <p>{res.rating}</p>
+                </li>
+              )
+            })
+          }
+          </ul>
         </main>
       </div>
     );
